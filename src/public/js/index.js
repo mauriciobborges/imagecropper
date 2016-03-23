bd75b92ab982fe1589e268fe54a8783f799dad79
tefferson <teffersonguterres@gmail.com>
2016-03-23 10:56:45 -0300

    feat(index.html): fazer download da imagem editada.

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,63 +1,65 @@
 'use strict';
 
+var $uploadCrop;
 
+function readFile(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-
-    var $uploadCrop;
-
-    function readFile(input) {
-      if (input.files && input.files[0]) {
-              var reader = new FileReader();
-              
-              reader.onload = function (e) {
-                $uploadCrop.croppie('bind', {
-                  url: e.target.result
-                });
-              }
-              reader.readAsDataURL(input.files[0]);
-          }
-          else {
-            swal("Sorry - you're browser doesn't support the FileReader API");
-        }
-    }
-
-    $uploadCrop = $('#cropper').croppie({
-      viewport: {
-        width: 200,
-        height: 200,
-        type: 'circle'
-      },
-      boundary: {
-        width: 300,
-        height: 300
-      },
-      exif: true
-    });
-
-    $('#image').on('change', function () { readFile(this); });
-    $('#download').on('click', function (ev) {
-      $uploadCrop.croppie('result', {
-        type: 'canvas',
-        size: 'viewport'
-      }).then(function (resp) {
-        $('#result').prepend('<img id="theImg" src="theImg.png" />')
+    reader.onload = function (e) {
+      $uploadCrop.croppie('bind', {
+        url: e.target.result
       });
-    });
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+  else {
+    swal("Sorry - you're browser doesn't support the FileReader API");
+  }
+};
+
+$uploadCrop = $('#cropper').croppie({
+  viewport: {
+    width: 200,
+    height: 200,
+    type: 'circle'
+  },
+  boundary: {
+    width: 300,
+    height: 300
+  },
+  exif: true
+});
+
+$('#image').on('change', function () {
+  readFile(this);
+});
+$('#download').on('click', function (ev) {
+  $uploadCrop.croppie('result', {
+    type: 'canvas',
+    size: 'viewport'
+  }).then(function (resp) {
+    var dl = document.createElement('a');
+    dl.setAttribute('href', resp);
+    dl.setAttribute('download', 'ibagem.jpg');
+    dl.click();
+  });
+});
 /*
 $('#image').change(function(){
-  $('#cropper').croppie({
+$('#cropper').croppie({
 
-    url: '/img/image.jpg',
-    // viewport options
-    viewport: {
-      width: 200,
-      height: 200,
-      type: 'circle'
-    },
-    // boundary options
-    boundary: {
-      width: 300,
-      height: 300
-    }
-  });
+url: '/img/image.jpg',
+// viewport options
+viewport: {
+width: 200,
+height: 200,
+type: 'circle'
+},
+// boundary options
+boundary: {
+width: 300,
+height: 300
+}
+});
 });*/
