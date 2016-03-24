196ef95b1d3a75862c7c99491be416bd9f23c062
Eduardo Arnold <eduardo.arnold@outlook.com>
2016-03-24 15:51:55 -0300

    chore(dist): uglify, minify and create sourcemaps for JS files

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ const gulp = require('gulp');
 
 const gutil = require('gulp-util');
 const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+
 const jshint = require('gulp-jshint');
 const stylish = require('jshint-stylish');
+
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 // PATHS
 // Constants for folder paths in project
@@ -34,4 +40,19 @@ gulp.task('lint', () => {
       var errorMessage = chalk.yellow.bold.bgRed(error.message,'To many errors!!');
       gutil.log(errorMessage);
     });
+});
+
+// Uglify, Minify and generate Sourcemaps for JS files
+gulp.task('min:js', () => {
+  gulp.src(paths.dist)
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(uglify({
+        compress: {
+            negate_iife: false
+        }
+    }))
+    .pipe(rename('bundle.min.js'))
+    .pipe(sourcemaps.write('./dist'))
+    .pipe(gulp.dest('./dist'));
 });
